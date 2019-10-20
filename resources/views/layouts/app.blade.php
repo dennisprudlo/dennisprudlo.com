c267c14a23dab5b01b3fb27915dd91863fdf2011
Dennis Prudlo <dennis.prudlo@outlook.com>
2019-10-21 00:44:10 +0200

    Add service worker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,5 +49,13 @@
 
 		<script src="{!! asset('js/dennisprudlo.min.js') !!}" charset="utf-8"></script>
 		@yield('scripts')
+
+		<script type="text/javascript">
+			if ('serviceWorker' in navigator) {
+				window.addEventListener('load', function() {
+					navigator.serviceWorker.register('js/pwa/service-worker.js')
+				});
+			}
+		</script>
 	</body>
 </html>
