b362aaa190c232039d78ce7c601875d7e0283352
Dennis Prudlo <mail@dennisprudlo.com>
2019-11-22 21:15:49 +0100

    Add image feed

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
 	public function getIndex() {
-		return view('index');
+		$portfolio = [
+			// 'HTW Mate' => (object) [
+			//
+			// ]
+		];
+
+		$images = [
+			'https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/64887803_514027852470265_8776174369259711903_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=101&oh=862eca592f74308311ac736a71028940&oe=5E67DFE1' => Carbon::create(2019,  6, 30, 13, 24, 55, 'Europe/Berlin'),
+			'https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/64531051_2336896319858748_1414173876376209719_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=100&oh=8b7c33584a7d44b6389dbc92a78694c3&oe=5E859ADD' => Carbon::create(2019,  6, 23, 19, 12, 55, 'Europe/Berlin'),
+			'https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/53034702_331999484000364_4198757688388099132_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=100&oh=29aa501e392cde66436c365368413635&oe=5E815A33' => Carbon::create(2019,  3, 23, 18, 59, 04, 'Europe/Berlin'),
+			'https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/e35/53539954_1129142043913522_1585111614850325282_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=110&oh=8d1952a0875dd246138677b0b44ca06d&oe=5E73FD42' => Carbon::create(2019,  3, 10, 19, 05, 48, 'Europe/Berlin'),
+			'https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/40547060_2096578727258063_3543945352431595695_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=111&oh=97f8059f787d7ab19fe96316d83feaab&oe=5E701CA7' => Carbon::create(2018,  9,  9, 19, 05, 48, 'Europe/Berlin'),
+		];
+
+		return view('index')
+			->withPortfolio($portfolio)
+			->withImages($images);
 	}
 
 	public function getMasthead() {
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,20 +5,30 @@
 	<div class="section" id="about">
 		<div class="layout__sheet">
 			<h1>Hi, I am Dennis Prudlo</h1>
-			<p>I am a highly motivated full stack web & iOS developer based in the heart of Europe – Berlin. I started to learn programming on my own at age 13. Curious about new technologies and new possibilities in development I improved my skills in various languages since then.<p>
-			<p>While developing websites I found out that I am very concerned about the overall design and usability. Turns out, I am a UI/UX enthusiast. User experience is the name of the game.</p>
+			<p>...a highly motivated <span class="-highlight">full stack web & iOS developer</span> based in the heart of Europe – Berlin. I started to learn programming on my own at age 13. Curious about new technologies and new possibilities in development I improved my skills in various languages since then.</p>
+			<p>While developing websites and mobile applications I found out that I am very concerned about the overall design and usability. Turns out, I am a <span class="-highlight">UI/UX enthusiast</span>. User experience is the name of the game.</p>
 		</div>
 	</div>
 	<div class="section" id="portfolio">
 		<h1 class="layout__sheet">Portfolio</h1>
 		<div class="layout__extended-sheet">
-
+			@foreach ($portfolio as $name => $properties)
+				{{ $name }}
+			@endforeach
 		</div>
 	</div>
 
 	<div class="section" id="feed">
 		<div class="layout__extended-sheet">
-
+			<div class="feed">
+				@foreach ($images as $url => $date)
+					<div class="feed__image" style="background-image: url({{ $url }})">
+						<div class="feed__image-title">
+							<span class="interaction--no-select">{{ $date->isoFormat('MMM D') }}</span>
+						</div>
+					</div>
+				@endforeach
+			</div>
 		</div>
 	</div>
 	@include('layouts.components.footer')
