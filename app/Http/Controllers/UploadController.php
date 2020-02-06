497e568081e702207d33097898f9781caeb23f19
Dennis Prudlo <mail@dennisprudlo.com>
2020-02-06 15:37:00 +0100

    Remove size restrictions

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -12,6 +12,11 @@ class UploadController extends Controller
 	}
 
 	public function postUpload(Request $request) {
+		ini_set('memory_limit', '-1');
+		ini_set('upload_max_filesize', '799M');
+		ini_set('post_max_size', '800M');
+		ini_set('max_file_uploads', '1');
+
 		$request->file('uploadFile')->storeAs('uploads', now()->timestamp.'-'.$request->file('uploadFile')->getClientOriginalName());
 
 		return view('general.upload')->with('uploaded', true);
